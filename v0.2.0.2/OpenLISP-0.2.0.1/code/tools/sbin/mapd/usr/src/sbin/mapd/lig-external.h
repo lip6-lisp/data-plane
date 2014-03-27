04cde7ffbbea61e4792b7453cdd22b8ddc618999
lip6-lisp <stefano.secci@lip6.fr>
2014-03-27 11:20:21 +0100

    Version 0.2.0.2 now support for FreeBSD 9.2 and 10.0.

---
+++ v0.2.0.2/OpenLISP-0.2.0.1/code/tools/sbin/mapd/usr/src/sbin/mapd/lig-external.h
@@ -0,0 +1,85 @@
+/*-/usr/src/sbin/mapd/lig-external.h
+ *
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 4. Neither the name of the University nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * $Id: lig-external.h 185 2011-09-24 11:22:53Z ggx $
+ *
+ */
+
+/*
+ *	lig-external.h
+ *
+ *	By David Meyer <dmm@1-4-5.net>
+ *	Copyright 2009 David Meyer
+ *
+ *	Any externals that everyone needs go here.
+ *
+ *	David Meyer
+ *	dmm@1-4-5.net
+ *	Thu Apr  9 12:01:29 2009
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     o Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     o Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     o Neither the name of the University nor the names of its contributors
+ *       may be used to endorse or promote products derived from this software
+ *       without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *
+ *	$Header: /mnt/disk1/dmm/src/lig/RCS/lig-external.h,v 1.1 2010/11/14 20:47:00 dmm Exp $
+ *
+ */
+
+extern  unsigned int	debug;
+extern  unsigned int	disallow_eid;
+extern  unsigned int	udp_checksum_disabled;
+extern	ushort		emr_inner_src_port;
+
+#ifdef BSD
+#include <string.h>
+#else
+extern void		*memcpy();
+extern void		*memset();
+extern char		*strdup();
+#endif
+
+extern ushort		csum ();
+extern long		tvdiff();
---
+++ v0.2.0.2/README.txt
@@ -0,0 +1,21 @@
+1.  INTRODUCTION
+
+    The OpenLISP implements the data-plane of LISP (Locator/Identifier Separation Protocol).
+    Please refer to the IETF LISP drafts for a description of the LISP data plane and 
+    related functionalities.	
+    
+    This is a modified version of OpenLISP (http://www.openlisp.org/) to support load balancing and two new functions: 
+    LISP Proxy Tunnel Router (PxTR) and Reencapsulating Tunnel Router (RTR). 
+
+    Now added support for FreeBSD 9.2 and 10.0.
+
+
+2.  INSTALL
+
+    Please refer to documents inside the tarball source code.
+
+3.  FUNCTION SWITCHING
+
+    Two new sysctl variables "net.lisp.function" and "net.lisp.xtr_te" have been added to OpenLISP to allow switching between 
+    functions and enable LISP-TE function on xTR. 
+    With "net.lisp.function" there are three available values: xtr, pxtr and rtr		
